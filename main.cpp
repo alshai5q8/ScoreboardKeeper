79a340f6858f7ac813d5106ce9492eb4000e41e2
salshaikh <salshaikh@mail.csuchico.edu>
2018-04-09 21:13:04 -0700

    Scoreboard

--- main.cpp
+++ main.cpp
@@ -7,29 +7,57 @@ const int MAX_PERIODS = 10;
 const int MAX_TEAMS = 4;
 const int MIN_PERIODS = 1;
 const int MIN_TEAMS = 1;
-
+vector< vector<int> > Scoreboard;
 int main()
 {
-  int periods;
-  int teams;
+        int periods;
+        int teams;
 
-  cout<<"How many competitors? ";
-  cin>>teams;
-  cout<<"How many scoring periods? ";
-  cin>>periods;
+        cout<<"How many competitors? ";
+        cin>>teams;
+        cout<<"How many scoring periods? ";
+        cin>>periods;
 
-  if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
-      teams > MAX_TEAMS || periods > MAX_PERIODS )
-  {
-    cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
-    cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
-    return 0;
-  }
-  else
-  {
-   //make scoreboard and fill it with zeros
-  
-   //once created, display the scoreboard
-  }
-  return 0;
+        if( teams < MIN_TEAMS || periods < MIN_PERIODS || 
+                        teams > MAX_TEAMS || periods > MAX_PERIODS )
+        {
+                cout<<"Must have between "<<MIN_TEAMS<<" and "<<MAX_TEAMS<<" competitors.\n";
+                cout<<"and between "<<MIN_PERIODS<<" and "<<MAX_PERIODS<<" periods.\n";
+                return 0;
+        }
+        else
+        {
+                //make scoreboard and fill it with zeros
+               
+               Scoreboard.resize(teams);
+
+                for(int row = 0; row < Scoreboard.size(); row++)
+                {       
+                        Scoreboard[row].resize(periods);
+
+                        for(int col = 0; col < Scoreboard[row].size(); col++)
+                        {       
+                                Scoreboard[row][col] = 0;
+                        }
+                }
+
+
+                cout<<"SCOREBOARD"<<endl; 
+                for(int r = 0; r < teams; r++)
+                {       
+                        cout<<"Player "<<r + 1<<": ";
+
+                        for(int c = 0; c < periods; c++)
+                        {
+
+                                cout<<Scoreboard[r][c]<<"|";
+
+                        }
+
+                        cout<<endl;
+
+                }
+                //once created, display the scoreboard
+        }
+        return 0;
 }
